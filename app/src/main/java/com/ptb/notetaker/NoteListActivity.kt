0b8ba7afc2b794f1f2b6484117852078b4e10b86
Paul Bagar <Paul.Bagar@independenthealth.com>
2024-03-29 13:10:56 -0400

    Almost done

---
+++ app/src/main/java/com/ptb/notetaker/NoteListActivity.kt
@@ -0,0 +1,44 @@
+package com.ptb.notetaker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.ptb.notetaker.databinding.ActivityNoteListBinding
+
+class NoteListActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_note_list)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fabAddNote.setOnClickListener { view ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_note_list)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
